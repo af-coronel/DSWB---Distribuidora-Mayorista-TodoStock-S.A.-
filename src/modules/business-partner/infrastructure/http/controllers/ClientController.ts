245acefe80f1a7993f1bf378f59fd30ce97d6f8c
Valeria Mansueto <vale-mansueto@hotmail.com>
2026-04-24 22:09:28 -0300

    feat: add controller to client

---
+++ src/modules/business-partner/infrastructure/http/controllers/ClientController.ts
@@ -0,0 +1,49 @@
+import { RegisterBusinessPartner } from "../../../application/use-cases/RegisterBusinessPartner.js";
+import type { IBusinessPartner } from "../../../domain/interfaces/IBusinessPartner.js";
+import type { Request, Response } from "express";
+export class ClientController {
+  constructor(private registerUseCase: RegisterBusinessPartner) {}
+
+  async create(req: Request, res: Response) {
+    try {
+      const {
+        cuit,
+        legal_name,
+        phone,
+        email,
+        legal_address,
+        vat_condition,
+        customer_data,
+      } = req.body;
+
+      const newClient: IBusinessPartner = {
+        cuit,
+        legal_name,
+        phone,
+        email,
+        legal_address,
+        vat_condition,
+        active: true,
+        type: ["CLIENT"], // Forzamos el tipo
+        customer_data: customer_data || { credit_limit: 0, current_balance: 0 },
+        vendor_data: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+        created_by: "system_admin", // TODO: actualizar con la info del middleware de Auth
+        updated_by: "system_admin", // TODO: actualizar con la info del middleware de Auth
+      };
+
+      await this.registerUseCase.execute(newClient);
+
+      return res.status(201).json({
+        message: "Cliente registrado exitosamente",
+        cuit: newClient.cuit,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        error: true,
+        message: error.message,
+      });
+    }
+  }
+}
