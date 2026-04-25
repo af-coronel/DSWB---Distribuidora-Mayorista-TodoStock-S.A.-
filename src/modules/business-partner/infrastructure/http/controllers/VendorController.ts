b67bc302a464d9ac8ddc96ff9f1503e0b23a4450
Valeria Mansueto <vale-mansueto@hotmail.com>
2026-04-24 22:14:45 -0300

    feat: add controller to vendor

---
+++ src/modules/business-partner/infrastructure/http/controllers/VendorController.ts
@@ -0,0 +1,54 @@
+import type { Request, Response } from "express";
+import type { IBusinessPartner } from "../../../domain/interfaces/IBusinessPartner.js";
+import { RegisterBusinessPartner } from "../../../application/use-cases/RegisterBusinessPartner.js";
+
+export class VendorController {
+  constructor(private registerUseCase: RegisterBusinessPartner) {}
+
+  async create(req: Request, res: Response) {
+    try {
+      const {
+        cuit,
+        legal_name,
+        phone,
+        email,
+        legal_address,
+        vat_condition,
+        vendor_data,
+      } = req.body;
+
+      // Construimos el objeto asegurando el rol VENDOR
+      const newVendor: IBusinessPartner = {
+        cuit,
+        legal_name,
+        phone,
+        email,
+        legal_address,
+        vat_condition,
+        active: true,
+        type: ["VENDOR"],
+        customer_data: null,
+        vendor_data: {
+          lead_time: vendor_data?.lead_time || 0,
+          category: vendor_data?.category || "General",
+        },
+        created_at: new Date(),
+        updated_at: new Date(),
+        created_by: "system_admin",
+        updated_by: "system_admin",
+      };
+
+      await this.registerUseCase.execute(newVendor);
+
+      return res.status(201).json({
+        message: "Proveedor registrado exitosamente",
+        cuit: newVendor.cuit,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        error: true,
+        message: error.message,
+      });
+    }
+  }
+}
