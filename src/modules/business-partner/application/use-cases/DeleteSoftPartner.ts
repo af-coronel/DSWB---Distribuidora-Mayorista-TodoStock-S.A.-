d84288716378b0d4e8b8484d1c9dc6181dad87a2
af-coronel <alvaro.coronel@mi.unc.edu.ar>
2026-04-28 23:41:34 -0300

    refactor(business-partner): agregar userId al caso de uso DeleteSoftPartner para garantizar auditoria

--- src/modules/business-partner/application/use-cases/DeleteSoftPartner.ts
+++ src/modules/business-partner/application/use-cases/DeleteSoftPartner.ts
@@ -6,17 +6,17 @@ import type {
 export class DeleteSoftPartner {
   constructor(private partnerRepository: IBusinessPartnerRepository) {}
 
-  async execute(cuit: string): Promise<void> {
+  async execute(cuit: string, userId: string): Promise<void> {
     const partner = await this.partnerRepository.findByCuit(cuit);
 
     if (!partner) {
       throw new Error("Socio de negocio no encontrado");
     }
 
-    // Regla de negocio: Soft Delete
+    // Regla de negocio: Soft Delete y Auditoria
     partner.active = false;
     partner.updated_at = new Date();
-    partner.updated_by = "system_admin"; // Esto vendrá luego de la sesión
+    partner.updated_by =  userId;
 
     await this.partnerRepository.save(partner);
   }
